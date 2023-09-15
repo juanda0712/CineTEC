a96a4ca77405729820a22049218998183cb21c73
Juan Rodriguez <juanrm071299@gmail.com>
2023-09-14 23:51:59 -0600

    ejemplo modelado simple

---
+++ CineTECApiBackend/Controllers/PeliculasController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CineTECApiBackend.Models;
+
+
+namespace CineTECApiBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeliculasController : ControllerBase
+    {
+        private readonly List<Pelicula> peliculas;
+        public PeliculasController(List<Pelicula> peliculas)
+        {
+            this.peliculas = peliculas;
+        }
+
+        /// <summary>
+        /// Obtiene la lista de películas en formato JSON en lowerCamelCase.
+        /// </summary>
+        /// <returns>Una lista de películas.</returns>
+        [HttpGet]
+        public IActionResult ObtenerPeliculas()
+        {
+            // Tu lógica para obtener las películas aquí
+
+            return Ok(peliculas); // Devuelve la lista de películas en formato JSON en lowerCamelCase
+        }
+    }
+}
--- CineTECApiBackend/Program.cs
+++ CineTECApiBackend/Program.cs
@@ -1,8 +1,21 @@
+using CineTECApiBackend.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+List<Pelicula> peliculas = JsonSerializer.Deserialize<List<Pelicula>>(File.ReadAllText("Peliculas.json"));
+builder.Services.AddSingleton<List<Pelicula>>(peliculas);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
