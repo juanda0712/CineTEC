b6080e9e3523652d7cff21d1887c008ff0d4107c
Juan Rodriguez <juanrm071299@gmail.com>
2023-09-16 10:54:18 -0600

    Create Sala and SalaProyeccion models withj endpoint of union

---
+++ CineTECApiBackend/Models/Sala.cs
@@ -0,0 +1,10 @@
+﻿namespace CineTECApiBackend.Models
+{
+    public class Sala
+    {
+        public int IDSala { get; set; }
+        public int NumFilas { get; set; }
+        public int NumColumnas { get; set; }
+        public string NombreSucursal { get; set; } //FK de Sala (PK de Sucursal)
+    }
+}
---
+++ CineTECApiBackend/Models/SalaProyeccion.cs
@@ -0,0 +1,8 @@
+﻿namespace CineTECApiBackend.Models
+{
+    public class SalaProyeccion
+    {
+        public int IDSala {  get; set; } //Pk de Sala
+        public int IDProyeccion { get; set; } //PK de Proyeccion
+    }
+}
