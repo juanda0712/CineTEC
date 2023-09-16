e53e4737b0112abacf3690e5c3d72f81638bc6ef
valeriam07 <47421582+valeriam07@users.noreply.github.com>
2023-09-16 11:15:01 -0600

    Frontend initialization

--- cinetec-web-app/src/app/app.routes.ts
+++ cinetec-web-app/src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { BootstrapTestComponent } from 'src/views/bootstrap-test/bootstrap-test.component';
+import { HomeComponent } from 'src/views/bootstrap-test/home.component';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: '', redirectTo: "/home", pathMatch: 'full'
+
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    title: 'Home',
+  },
+  {
+    path: 'bootstrap',
     component: BootstrapTestComponent,
     title: 'Bootstrap',
   },
