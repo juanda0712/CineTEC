9c2de17b8df97c504a6c64a5a60fd941c9949763
Juan Rodriguez <juanrm071299@gmail.com>
2023-09-17 02:11:14 -0600

    Modify structure and styles with bootstrap 5

--- cinetec-web-app/src/app/Components/listings/listings.component.ts
+++ cinetec-web-app/src/app/Components/listings/listings.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MovieCardComponent } from './movie-card/movie-card.component';
-import { NavBarComponent } from '../../Reusables/nav-bar/nav-bar.component';
+import { MovieCardComponent } from '../../Reusables/movie-card/movie-card.component';
 
 @Component({
   selector: 'app-listings',
   standalone: true,
-  imports: [CommonModule, MovieCardComponent, NavBarComponent],
+  imports: [CommonModule, MovieCardComponent],
   templateUrl: './listings.component.html',
   styles: [],
 })
