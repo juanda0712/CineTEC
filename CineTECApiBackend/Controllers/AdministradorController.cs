7cf0f2a06f6a5b742ec1d874653ee3902b76d67e
Juan Rodriguez <juanrm071299@gmail.com>
2023-09-19 23:27:00 -0600

    administrador relation

---
+++ CineTECApiBackend/Controllers/AdministradorController.cs
@@ -0,0 +1,48 @@
+﻿using CineTECApiBackend.Models;
+using CineTECApiBackend.Utilities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CineTECApiBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdministradorController : ControllerBase
+    {
+        private readonly IJsonDataManager _jsonDataManager;
+        public AdministradorController(IJsonDataManager jsonDataManager)
+        {
+            _jsonDataManager = jsonDataManager;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] Administrador credentials)
+        {
+            try
+            {
+                // Cargar la lista de administradores desde el archivo JSON
+                var allAdmins = _jsonDataManager.LoadJsonFile<Administrador>("Administradores.json");
+
+                // Verificar si las credenciales son válidas
+                var administrador = allAdmins.FirstOrDefault(a => a.Usuario == credentials.Usuario && a.Password == credentials.Password);
+
+                if (administrador != null)
+                {
+                    // Las credenciales son válidas, aca es donde se retorna un token de autenticación.
+                    // Aquí simplemente retornamos un mensaje de éxito.
+                    return Ok(new { status = "ok" });
+                }
+                else
+                {
+                    // Credenciales no válidas, retorna un mensaje de error.
+                    return BadRequest("Incorrect Credentials");
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Error en el servidor: {ex.Message}");
+            }
+        }
+
+
+    }
+}
---
+++ CineTECApiBackend/Models/Administrador.cs
@@ -0,0 +1,8 @@
+﻿namespace CineTECApiBackend.Models
+{
+    public class Administrador
+    {
+        public string Usuario { get; set; }
+        public string Password { get; set; }
+    }
+}
